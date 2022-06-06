48dc9de297d15fd5eaceab62c5e7b8a876d865f5
mehedihasanpiash <piash599@gmail.com>
2022-06-06 22:31:45 +0600

    - update local db ui and other improvements

--- MVVM_Hilt/app/src/main/java/com/piashcse/experiment/mvvm_hilt/ui/roomdb/adddata/AddUserFragment.kt
+++ MVVM_Hilt/app/src/main/java/com/piashcse/experiment/mvvm_hilt/ui/roomdb/adddata/AddUserFragment.kt
@@ -1,37 +1,19 @@
 package com.piashcse.experiment.mvvm_hilt.ui.roomdb.adddata
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.piashcse.experiment.mvvm_hilt.databinding.FragmentAddUserBinding
 import com.piashcse.experiment.mvvm_hilt.data.datasource.local.room.User
+import com.piashcse.experiment.mvvm_hilt.utils.base.BaseBindingFragment
 import com.piashcse.experiment.mvvm_hilt.utils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class AddUserFragment : Fragment() {
-    private var _binding: FragmentAddUserBinding? = null
-    private val binding get() = requireNotNull(_binding) // or _binding!!
+class AddUserFragment : BaseBindingFragment<FragmentAddUserBinding>() {
     private val addUserViewModel: AddUserViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        _binding = FragmentAddUserBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun init() {
         initView()
-
     }
 
     private fun initView() {
@@ -40,8 +22,8 @@ class AddUserFragment : Fragment() {
                 addUserViewModel.insertUser(
                     User(
                         0,
-                        binding.name.text.toString().capitalize(),
-                        binding.company.text.toString().capitalize()
+                        name.text.toString().capitalize(),
+                        company.text.toString().capitalize()
                     )
                 )
                 it.findNavController().popBackStack()
@@ -52,10 +34,5 @@ class AddUserFragment : Fragment() {
                 it.hideKeyboard()
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
