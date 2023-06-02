e3ab5a1fd3e7a2cc2b58193cedf309d5ce2e63a6
mehedihasanpiash <piash599@gmail.com>
2023-06-02 12:56:20 +0600

    - Update gradle dependency

--- MVVM_Hilt/app/src/main/java/com/piashcse/experiment/mvvm_hilt/HiltApplication.kt
+++ MVVM_Hilt/app/src/main/java/com/piashcse/experiment/mvvm_hilt/HiltApplication.kt
@@ -1,5 +1,6 @@
 package com.piashcse.experiment.mvvm_hilt
 
+import androidx.multidex.BuildConfig
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
